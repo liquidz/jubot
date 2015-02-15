e580a54705dbb943825d0bdd6f898d9ec6476786
liquidz <liquidz.uo@gmail.com>
2015-02-15 14:36:42 +0900

    add docstring to jubot.scheduler

--- src/jubot/scheduler.clj
+++ src/jubot/scheduler.clj
@@ -1,19 +1,44 @@
 (ns jubot.scheduler
+  "Jubot scheduler."
   (:require
     [com.stuartsierra.component :as component]
     [cronj.core :as c]))
 
-(def ^:const SCHEDULE_REGEXP #"^.*-schedule$")
+(def ^{:const true
+       :doc "The regular expression for collecting schedules automatically."}
+  SCHEDULE_REGEXP #"^.*-schedule$")
 
 (defn schedule
+  "Generate a schedule from a pair of cronj format string and function.
+
+  Params
+    cron-expr - Cronj format string. http://docs.caudate.me/cronj/#crontab
+    f         - A function with zero parameter.
+  Return
+    A schedule function.
+  "
   [cron-expr f]
   (with-meta f {:schedule cron-expr}))
 
 (defn schedules
+  "Generate sequence of schedules from pairs of cronj format string and function.
+
+  Params
+    args - Pairs of cronj format string and function.
+  Return
+    Sequence of schedule functions.
+  "
   [& args]
   (map #(apply schedule %) (partition 2 args)))
 
 (defn- schedule->task
+  "Convert a schedule function to cronj task.
+
+  Params
+    f - A schedule function.
+  Return
+    A cronj task.
+  "
   [f]
   {:id       (str (gensym "task"))
    :handler  (fn [t opt] (f))
@@ -37,11 +62,25 @@
           (assoc this :cj nil)))))
 
 (defn create-scheduler
-  [config-option]
+  "Create the scheduler.
+
+  Params
+    :entries - Sequence of schedules.
+  Return
+    Scheduler component.
+  "
+  [{:keys [entries] :as config-option}]
   (map->Scheduler (merge {:entries []}
                          config-option)))
 
 (defn public-schedules
+  "Return sequence of public schedule vars which matched SCHEDULE_REGEXP in specified namespaces.
+
+  Params
+    ns-regexp - A regular expression which specifies namespaces for searching schedules.
+  Return
+    Sequence of schedule vars.
+  "
   [ns-regexp]
   (->> (all-ns)
        (filter #(re-find ns-regexp (str (ns-name %))))
@@ -49,5 +88,12 @@
        (filter #(re-matches SCHEDULE_REGEXP (-> % meta :name str)))))
 
 (defn collect
+  "Return sequence of public schedules in specified namespaces.
+
+  Params
+    ns-regexp - A regular expression which specifies namespaces for searching schedules.
+  Return
+    Sequence of schedules.
+  "
   [ns-regexp]
   (flatten (map var-get (public-schedules ns-regexp))))
