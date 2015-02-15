7d75e7dc94c1d7effa847b5184933670890ed135
liquidz <liquidz.uo@gmail.com>
2015-02-15 11:10:31 +0900

    add docstring to jubot.handler in japanese

--- src/jubot/handler.clj
+++ src/jubot/handler.clj
@@ -1,9 +1,20 @@
 (ns jubot.handler
+  "ハンドラーのユーティリティ"
   (:refer-clojure :exclude [comp]))
 
-(def ^:const HANDLER_REGEXP #"^.*-handler$")
+(def ^{:const true :doc "自動で収集する対象ハンドラー名の正規表現"}
+  HANDLER_REGEXP #"^.*-handler$")
 
 (defn regexp
+  "正規表現と関数のリストからハンドラー関数を生成し返す
+
+  Params
+    reg-fn-list - 正規表現と関数のペア
+                  正規表現にマッチした入力があった場合に、対応する関数が呼び出される
+                  関数にはハンドラーへの入力に加えて正規表現にマッチした結果(re-findの戻り値)を :match として追加したものが渡される
+  Return
+    ハンドラー関数
+  "
   [& reg-fn-list]
   {:pre [(zero? (mod (count reg-fn-list) 2))]}
 
@@ -20,6 +31,13 @@
       (partition 2 reg-fn-list))))
 
 (defn comp
+  "ハンドラー関数を合成する
+
+  Params
+    fs - ハンドラー関数のシーケンス
+  Return
+    合成されたハンドラー関数
+  "
   ([] identity)
   ([& fs]
    {:pre [(every? #(or (fn? %) (var? %)) fs)]}
@@ -31,6 +49,13 @@
            ret))))))
 
 (defn public-handlers
+  "指定されたネームスペース内で HANDLER_REGEXP にマッチするパブリックなハンドラー関数のシーケンスを返す
+
+  Params
+    ns-regexp - ハンドラー関数検索対象のネームスペースの正規表現
+  Return
+    ハンドラー関数のシーケンス
+  "
   [ns-regexp]
   (->> (all-ns)
        (filter #(re-find ns-regexp (str (ns-name %))))
@@ -38,6 +63,13 @@
        (filter #(re-matches HANDLER_REGEXP (-> % meta :name str)))))
 
 (defn collect
+  "指定されたネームスペース内で HANDLER_REGEXP にマッチするパブリックなハンドラー関数を合成して返す
+
+  Params
+    ns-regexp - ハンドラー関数検索対象のネームスペースの正規表現
+  Return
+    ハンドラー関数
+  "
   [ns-regexp]
   (if-let [handlers (seq (public-handlers ns-regexp))]
     (apply comp handlers)
