5e31a3513436311d12277bc3b04242eabc15b511
liquidz <liquidz.uo@gmail.com>
2015-02-15 10:40:00 +0900

    add docstring to jubot.brain

--- src/jubot/brain.clj
+++ src/jubot/brain.clj
@@ -1,4 +1,5 @@
 (ns jubot.brain
+  "Jubot brain manager."
   (:require
     [jubot.system       :refer [system]]
     [jubot.brain.memory :refer [map->MemoryBrain]]
@@ -6,15 +7,37 @@
   (:refer-clojure :exclude [set get]))
 
 (defn create-brain
+  "Create the specified brain.
+
+  Params
+    :brain - a brain's name
+  Return
+    brain component
+  "
   [{:keys [brain] :as config-option}]
   (case brain
     "redis" (map->RedisBrain config-option)
     (map->MemoryBrain config-option)))
 
 (defn set
+  "Store data to system brain.
+  Before using this function, jubot.system should be started.
+
+  Params
+    k - key
+    v - value
+  "
   [k v]
   (some-> system :brain :set (as-> f (f k v))))
 
 (defn get
+  "Get data from system brain.
+  Before using this function, jubot.system should be started.
+
+  Params
+    k - key
+  Return
+    stored data
+  "
   [k]
   (some-> system :brain :get (as-> f (f k))))
