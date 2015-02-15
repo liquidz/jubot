b769ac078cfa0c8f9313919cbc775b1d8d681903
liquidz <liquidz.uo@gmail.com>
2015-02-15 13:36:53 +0900

    add docstring for jubot.require

--- src/jubot/require.clj
+++ src/jubot/require.clj
@@ -1,9 +1,15 @@
 (ns jubot.require
+  "Jubot utilities for requiring namespaces."
   (:require
     [clojure.java.io              :as io]
     [clojure.tools.namespace.find :as find]))
 
 (defn regexp-require
+  "Require namespaces which matches specified regular expression.
+
+  Params
+    ns-regexp - A regular expression which specifies namespaces.
+  "
   [ns-regexp]
   (doseq [sym (->> (find/find-namespaces-in-dir (io/file "."))
                    (filter #(re-find ns-regexp (str %)))
