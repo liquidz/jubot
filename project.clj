f24a8a8d8f63d1dd1a6cbe7f9195ba2bb22065e6
liquidz <liquidz.uo@gmail.com>
2015-02-22 14:31:18 +0900

    inc ver

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jubot "0.0.1-SNAPSHOT-10"
+(defproject jubot "0.0.1-SNAPSHOT-11"
   :description "Chatbot framework in Clojure"
   :url "https://github.com/liquidz/jubot"
   :license {:name "Eclipse Public License"
