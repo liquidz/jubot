5a7947dd323a42b4c2192207accab6dccfd527f0
liquidz <liquidz.uo@gmail.com>
2015-05-06 20:02:38 +0900

    update dependent libraries

--- project.clj
+++ project.clj
@@ -1,19 +1,19 @@
-(defproject jubot "0.0.1"
+(defproject jubot "0.1.0"
   :description "Chatbot framework in Clojure"
   :url "https://github.com/liquidz/jubot"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure         "1.6.0"]
-                 [com.stuartsierra/component  "0.2.2"]
+                 [com.stuartsierra/component  "0.2.3"]
                  [org.clojure/tools.namespace "0.2.10"]
                  [org.clojure/tools.cli       "0.3.1"]
-                 [compojure                   "1.3.2"]
+                 [compojure                   "1.3.3"]
                  [ring/ring-jetty-adapter     "1.3.2"]
                  [ring/ring-defaults          "0.1.4"]
-                 [org.clojure/data.json       "0.2.5"]
-                 [com.taoensso/carmine        "2.9.0"]
-                 [com.taoensso/timbre         "3.3.1"]
+                 [org.clojure/data.json       "0.2.6"]
+                 [com.taoensso/carmine        "2.10.0"]
+                 [com.taoensso/timbre         "3.4.0"]
                  [clj-http-lite               "0.2.1"]
                  [im.chit/cronj               "1.4.3"]]
 
